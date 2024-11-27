fbab6c1af812531f16604ffb972b0eac98447129
xZept <allenjames.laxamana@gmail.com>
2024-11-28 03:35:56 +0800

    Add tabs: receipt, inventory, and add item

--- app/src/main/java/com/example/inventoryandsalesmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/inventoryandsalesmanagementsystem/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.inventoryandsalesmanagementsystem;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TableLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +10,16 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    BottomAppBar bottomAppBar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,21 +31,15 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        bottomAppBar=findViewById(R.id.bottomAppBar);
-        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                if (item.getItemId()==R.id.inventory)
-                {
-                    // Invoke inventory view
-                }
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
 
-                if (item.getItemId()==R.id.receipt)
-                {
+        tabLayout.setupWithViewPager(viewPager);
 
-                }
-                return false;
-            }
-        });
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new FragmentReceipt(), "RECEIPT");
+        vpAdapter.addFragment(new FragmentInventory(), "INVENTORY");
+        vpAdapter.addFragment(new FragmentAddItem(), "ADD ITEM");
+        viewPager.setAdapter(vpAdapter);
     }
 }
